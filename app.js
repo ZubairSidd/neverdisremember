da75c6c57ec7563b96f5d7217c195d8fc14357aa
Zubair Siddique <zubairsidd20@gmail.com>
2021-06-18 11:18:19 +0530

    adding list in process

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const myList = []
+let input = document.querySelector("#list")
+const button = document.querySelector("#addButton")
+let myList = document.querySelector("#myList")
+let listData = []
+
+function addButtonClickHandler() {
+    
+    console.log(input.value);
+    if(input.value !== ""){
+        listData.push(input.value)
+    }
+    console.log(listData)
+    // let li = document.createElement('li')
+    // li.innerText = input.value;
+    // myList.appendChild(li);
+    input.value = "";
+    listData.forEach((item) => {
+        let li = document.createElement('li')
+        li.innerHTML = item;
+        myList.appendChild(li);
+    })
+}
+
+button.addEventListener("click", addButtonClickHandler)
