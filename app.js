07a751e2a1defe2b9b577d592b7c4e6ce730c058
Zubair Siddique <zubairsidd20@gmail.com>
2021-06-18 11:50:50 +0530

    to do list working

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 let input = document.querySelector("#list")
 const button = document.querySelector("#addButton")
 let myList = document.querySelector("#myList")
-let listData = []
+// let listData = []
 
 function addButtonClickHandler() {
-    
-    console.log(input.value);
-    if(input.value !== ""){
-        listData.push(input.value)
+
+    // console.log(input.value);
+    if (input.value !== "") {
+        // listData.push(input.value)
+        let li = document.createElement('li')
+        let btn = document.createElement('button')
+        btn.innerHTML = "remove";
+        li.innerHTML = input.value;
+        myList.appendChild(li);
+        myList.appendChild(btn);
+        btn.addEventListener("click", function(){
+            myList.removeChild(li);
+            myList.removeChild(btn)
+        })
     }
     console.log(listData)
-    // let li = document.createElement('li')
-    // li.innerText = input.value;
-    // myList.appendChild(li);
+
     input.value = "";
-    listData.forEach((item) => {
-        let li = document.createElement('li')
-        li.innerHTML = item;
-        myList.appendChild(li);
-    })
+    // listData.forEach((item) => {
+    //     let li = document.createElement('li')
+    //     li.innerHTML = item;
+    //     myList.appendChild(li);
+    // })
 }
 
 button.addEventListener("click", addButtonClickHandler)
