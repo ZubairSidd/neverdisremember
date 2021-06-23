b58cb47a436d0c929eb20da51710af80615d07bf
Zubair Siddique <zubairsidd20@gmail.com>
2021-06-23 16:32:30 +0530

    fixed button inside list

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ function addButtonClickHandler() {
         // listData.push(input.value)
         let li = document.createElement('li')
         let btn = document.createElement('button')
-        btn.innerHTML = "";
+        
         li.innerHTML = input.value;  
-        myList.appendChild(li);
-        myList.appendChild(btn);
-        // li.innerHTML = input.value + btn.outerHTML;
-        // myList.appendChild(li)
+        // myList.appendChild(li);
+        // myList.appendChild(btn);
+        li.appendChild(btn)
+        myList.appendChild(li)
         btn.addEventListener("click", function(){
             myList.removeChild(li);
             myList.removeChild(btn);
